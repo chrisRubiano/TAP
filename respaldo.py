aa612dd404a1179c51f41346b3a29b11e273ed75
chrisRubiano <chris.rubiano42@gmail.com>
2016-02-18 17:48:47 -0700

    copia directorios

--- respaldo.py
+++ respaldo.py
@@ -4,6 +4,8 @@ Autor: Cristian Samaniego
 Fecha: 17/02/2016
 
 Respaldamos un directorio origen a un directorio destino
+TODO:   +copiar cuando solo es un archivo
+        +Renombrar el respaldo cuando la carpeta destino ya existe
 '''
 import os
 import argparse
@@ -46,6 +48,10 @@ def total_file_list(absPath):
     return totalFileList
 
 
+def file_copy(absPathOri, absPathRes):
+    shutil.copytree(absPathOri, absPathRes)
+
+
 def main(origen, respaldo):
     """
 
@@ -56,13 +62,17 @@ def main(origen, respaldo):
     absPathRes = os.path.abspath(respaldo)
     existsOrigen = dir_exists(absPathOri)
     existsRespaldo = dir_exists(absPathRes)
-    if existsRespaldo:
-        isDirResp = is_dir(absPathRes)
-        if not isDirResp:
-            print "la ruta destino es un archivo"
+    if existsOrigen:
+        if existsRespaldo:
+            isDirResp = is_dir(absPathRes)
+            if not isDirResp:
+                print "la ruta destino es un archivo"
+            else:
+                file_copy(absPathOri, absPathRes)
+        else:
+            file_copy(absPathOri, absPathRes)
     else:
-       os.makedirs(absPathRes,0755)
-
+        print "no existe el directorio de origen"
 
 
 if __name__ == '__main__':
