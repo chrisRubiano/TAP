e0d41f16ed3c502176aa969ec3c652705212932d
chrisRubiano <chris.rubiano42@gmail.com>
2016-02-23 17:53:55 -0700

    rename_file creado

--- respaldo.py
+++ respaldo.py
@@ -4,8 +4,7 @@ Autor: Cristian Samaniego
 Fecha: 17/02/2016
 
 Respaldamos un directorio origen a un directorio destino
-TODO:   +copiar cuando solo es un archivo
-        +Renombrar el respaldo cuando la carpeta destino ya existe
+TODO:   +renombrar archivos
 '''
 import os
 import argparse
@@ -20,9 +19,22 @@ def rename_dir(abspath):
         splitPath[1] = splitPath[1][:-1] + str(int(x)+1)
     else:
         splitPath[1] = splitPath[1]+"2"
-    #splitPath[1] = splitPath[1]+"x"
     absPath = os.path.join(splitPath[0], splitPath[1])
     return absPath
+
+
+def rename_file(path):
+    splitPath = string.rsplit(path, "/", 1)
+    filename = splitPath[1]
+    filepath = splitPath[0]
+    while file_exists(path):
+        x = filename[-1:]
+        if x.isdigit():
+            filename = filename[:-1] + str(int(x)+1)
+        else:
+            filename + "2"
+        fullPath = os.path.join(filepath, filename)
+    return fullPath
 
 
 def dir_exists(absPath):
@@ -32,6 +44,19 @@ def dir_exists(absPath):
     :return: True si existe el directorio, False si no existe
     """
     if os.path.exists(absPath):
+        ex = True
+    else:
+        ex = False
+    return ex
+
+
+def file_exists(absPath):
+    """
+
+    :param absPath: el absolute path del directorio
+    :return: True si existe el directorio, False si no existe
+    """
+    if os.path.exists(absPath+".tar.gz"):
         ex = True
     else:
         ex = False
@@ -63,9 +88,19 @@ def total_file_list(absPath):
 
 def dir_copy(absPathOri, absPathRes):
     #shutil.copytree(absPathOri, absPathRes)
-    os.makedirs(absPathRes)
+    if not dir_exists(absPathRes):
+        os.makedirs(absPathRes)
     os.chdir(absPathRes)
-    shutil.make_archive("respaldo", "gztar", absPathRes, absPathOri)
+    splitpath = splitPath = string.rsplit(absPathOri, "/", 1)
+    filename = splitpath[1]
+    fullpath = os.path.join(absPathRes, filename)
+    if not file_exists(fullpath):
+        shutil.make_archive(filename, "gztar", absPathRes, absPathOri)
+    else:
+        filename = rename_file(fullpath)
+        shutil.make_archive(filename, "gztar", absPathRes, absPathOri)
+
+
 
 
 def file_copy(absPathOri, absPathRes):
@@ -89,8 +124,6 @@ def main(origen, respaldo):
                 print "la ruta destino es un archivo"
             else:
                 if is_dir(absPathOri):
-                    while dir_exists(absPathRes):
-                        absPathRes = rename_dir(absPathRes)
                     dir_copy(absPathOri, absPathRes)
                 else:
                     file_copy(absPathOri, absPathRes)
