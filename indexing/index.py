7af3332e511b68ba2ded602782355c19f3555037
Cristian Samaniego <chris.rubiano42@gmail.com>
2016-03-16 17:54:55 -0700

    tokenize.py

---
+++ indexing/index.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+'''
+Created on Feb 29, 2016
+
+@author: fabianvzla
+'''
+import urllib2
+from bs4 import BeautifulSoup
+
+def get_url(html):
+    lista_urls=[]
+    sopa=BeautifulSoup(html,'html.parser')
+    lista_links=sopa.find_all(role='article')
+    #print (lista_links)
+    for link in lista_links:
+        a_list=link.find_all('a')
+        #print (a_list)
+        for a in a_list:
+            url=a.get('href')
+            lista_urls.append(url)
+        
+    return lista_urls
+
+def lee_archivo(archivo):
+    try:
+        with open(archivo, "r") as fh:
+            texto=fh.read()
+    except:
+        texo=" "
+    return texto
+
+def save_file(texto,nombre_archivo):
+    try:
+        with open(nombre_archivo, "w") as fh:
+            fh.write(texto)
+            print "se escribio %s" % nombre_archivo
+    except:
+        print "ruta invalida %s" % nombre_archivo
+        
+
+def get_html(url):
+    try:
+        respuesta=urllib2.urlopen(url)
+        html=respuesta.read()
+        respuesta.close()
+        print ("Se obtuvo hmtl de %s"%url)
+    except:
+        html=""
+        print ("no se obtuvo html de %s"%url)
+    return html
+
+def main():
+    archivo="wired.html"
+    url="http://www.wired.com"
+    HTML =get_html(url)
+    #print HTML
+    save_file(HTML, archivo)
+    # url_list=["http://www.wired.com", "http://www.theverge.com","http://www.techcrunch.com"]
+    url_list=get_url(HTML)
+    archivo_contador="contador.txt"
+    contador=int(lee_archivo("contador.txt"))
+    for url in url_list:
+        HTML=get_html(url)
+        get_url(HTML)
+        archivo="pagina_%03d"%contador
+        print "se obtuvo html de %s" % url
+        save_file(HTML,archivo)
+        contador+=1
+    contador_str=str(contador)
+    save_file(contador_str, archivo_contador)
+if __name__ == '__main__':  
+    main()
+
+
+#url="https://www.google.com"
+#respuesta=urllib2.urlopen(url)
+#html=respuesta.read()
+#respuesta.close()
+#print html 
---
+++ indexing/tokenize.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+'''
+Created on 21/02/2016
+
+@author: fcirett
+
+TODO: crear indice global
+'''
+import argparse
+from bs4 import BeautifulSoup
+import unicodedata
+import os
+
+
+def get_html_files():
+
+
+
+def remove_punctuation(text):
+    """
+    >>> remove_punctuation(u'something')
+    u'something'
+
+    >>> remove_punctuation(u'something.,:else really')
+    u'somethingelse really'
+    """
+    punctutation_cats = set(['Pc', 'Pd', 'Ps', 'Pe', 'Pi', 'Pf', 'Po'])
+    return ''.join(x for x in text
+                   if unicodedata.category(x) not in punctutation_cats)
+
+
+def lee_archivo_html(nombre_archivo):
+    try:
+        #carga los datos
+        with open(nombre_archivo) as fh:
+            html = fh.read()
+        print ("se leyo el archivo %s" % nombre_archivo)
+    except:
+        html = ""
+        print ("NO se pudo leer el archivo %s" % nombre_archivo)
+    return html
+
+
+def get_texto (soup):
+    html_body = soup.find_all(itemprop='articleBody')
+    try:
+        paragraphs = html_body[0].find_all('p')
+        raw_text_list = []
+        raw_text_list = raw_text_list + [p.get_text() for p in paragraphs]
+        clean_text_list = []
+        for t in raw_text_list:
+            clean_text_list.append( t )
+        texto  = u''.join( clean_text_list)
+    except Exception, e:
+        print " "
+    return texto
+
+
+def get_words (texto):
+    word_list = texto.split()
+    clean_word_list = []
+    for word in word_list:
+        clean_word_list.append( remove_punctuation( word ) )
+    return clean_word_list
+
+
+def update_dictionary( diccionario, lista_palabras):
+    for l in lista_palabras:
+        if l not in diccionario:
+            diccionario[l] = 1
+        else:
+            diccionario[l] += 1
+
+
+def make_index( diccionario,lista_palabras, id):
+    for posicion, palabra in enumerate(lista_palabras):
+        if palabra not in diccionario:
+            diccionario[palabra] = [ id, [posicion]]
+        else:
+            lista_temp = diccionario[palabra]
+            lista_temp[1].append(posicion)
+            diccionario[palabra] = lista_temp
+
+
+def main( archivo_html):
+    HTML  = lee_archivo_html(archivo_html)
+    sopa  = BeautifulSoup( HTML )
+    texto = get_texto(sopa)
+    #html_text = soup.get_text()
+
+    print texto
+
+    words = get_words (texto)
+    word_dict = {}
+    update_dictionary(word_dict, words)
+    print "diccionario: %d" % len(word_dict)
+
+    #print(word_dict)
+    for k, v in sorted(word_dict.items()):
+        print '%s: %d' % (k,v)
+        #print u'{0}: {1}'.format(k, v)
+    word_index = {}
+    make_index( word_index, words, 1)
+
+    print (word_index['they'])
+    lista_they = word_index['they']
+    indice = lista_they[1][0]
+    print words[indice]
+    print (word_index['Yesterday'])
+
+    archivos_html = get_html_files():
+    for doc,archivo in enumerate(archivos_html):
+        HTML = lee_archivo_html(archivo)
+        sopa = BeautifulSoup(HTML, 'html.parser')
+        texto = get_texto(sopa)
+        words = get_words(texto)
+        word_index = {}
+        make_index(word_index, words, doc)
+        print word_index
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--archivo", help="archivo HTML a leer")
+    args = parser.parse_args()
+    main( args.archivo )
