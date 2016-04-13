f2e6634ffac1c18bb6d6a82777ef4dcca19e009c
Cristian Samaniego <chris.rubiano42@gmail.com>
2016-04-13 13:52:59 -0700

    muchas weas

--- dolar/dolar.py
+++ dolar/dolar.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Links:
 Banamex:            http://portal.banamex.com.mx/c719_004/economiaFinanzas/es/home?xhost=http://www.banamex.com/
@@ -5,11 +6,25 @@ Bancomer:
 Banjercito:         http://www.banjercito.com.mx/index.jsp?hd_ligaContenido=Informacion_financiera/redirectInfFin.jsp?operacion=6
 Banco del bajio:    http://www.bb.com.mx/
 
-
+TODO:   try exception en cada metodo
+        escribir archivos
 '''
 import urllib2
 from bs4 import BeautifulSoup
 import time
+
+def acumula_valores(acu, dolar):
+    acu['compra'].append(dolar['compra'])
+    acu['venta'].append(dolar['venta'])
+
+def console_print(banco, dolar):
+    print banco+' : %.4f | %.4f' % (dolar['compra'], dolar['venta'])
+
+
+def write_file(fileName, dolar):
+    file_name = fileName+".csv"
+    with open(file_name, 'a') as fh:
+        fh.write(time.strftime("%d-%m-%Y")+' '+time.strftime("%H:%M:%S")+', %.4f, %.4f' % (dolar['compra'], dolar['venta']))
 
 
 def get_html(url):
@@ -17,7 +32,6 @@ def get_html(url):
         respuesta=urllib2.urlopen(url)
         html=respuesta.read()
         respuesta.close()
-        print ("Se obtuvo hmtl de %s"%url)
     except:
         html=""
         print ("no se obtuvo html de %s"%url)
@@ -26,7 +40,7 @@ def get_html(url):
 
 def get_value_banamex(html):
     dolar = {}
-    sopa=BeautifulSoup(html,'html.parser')
+    sopa = BeautifulSoup(html,'html.parser')
     minisopas = sopa.find(id='cotizaciones').find('div').find(id='monedas').find('tbody').findAll('tr') #len=2
     dolar['compra'] = float(minisopas[0].find('td').text)
     dolar['venta'] = float(minisopas[1].find('td').text)
@@ -34,8 +48,8 @@ def get_value_banamex(html):
 
 
 def get_value_banbajio(html):
-    dolar={}
-    sopa=BeautifulSoup(html,'html.parser')
+    dolar = {}
+    sopa = BeautifulSoup(html,'html.parser')
     minisopa = sopa.findAll('div', {'class' : ['mod_inFin_central_2_compra', 'mod_inFin_central_2_venta']})
     dolar['compra'] = float(minisopa[0].text)
     dolar['venta'] = float(minisopa[1].text)
@@ -43,14 +57,22 @@ def get_value_banbajio(html):
 
 
 def main():
-    url_banamex = 'http://portal.banamex.com.mx/c719_004/economiaFinanzas/es/home?xhost=http://www.banamex.com/'
-    html_banamex = get_html(url_banamex)
-    dolar_banamex = get_value_banamex(html_banamex)
-    print time.strftime("%d-%m-%Y")+' '+time.strftime("%H:%M:%S")+', %.4f, %.4f' % (dolar_banamex['compra'], dolar_banamex['venta'])
-    url_banbajio = 'http://www.bb.com.mx/'
-    html_banbajio = get_html(url_banbajio)
-    dolar_banbajio = get_value_banbajio(html_banbajio)
-    print time.strftime("%d-%m-%Y")+' '+time.strftime("%H:%M:%S")+', %.4f, %.4f' % (dolar_banbajio['compra'], dolar_banbajio['venta'])
+    acu = {'compra':[],'venta':[]}
+    #banamex
+    banco = 'banamex'
+    url = 'http://portal.banamex.com.mx/c719_004/economiaFinanzas/es/home?xhost=http://www.banamex.com/'
+    html = get_html(url)
+    dolar = get_value_banamex(html)
+    write_file(banco, dolar)
+    console_print(banco, dolar)
+    acumula_valores(acu, dolar)
+    #banbajio
+    url = 'http://www.bb.com.mx/'
+    html = get_html(url)
+    dolar = get_value_banbajio(html)
+    write_file(banco, dolar)
+    console_print(banco, dolar)
+    acumula_valores(acu, dolar)
 
 if __name__ == '__main__':
     main()
