54bf0b142e06a3a4d1366042dd214fe4e87aef2c
Jonathan Guillermo <hernandezmarquina@gmail.com>
2016-04-27 11:34:20 -0700

    ftp tests

---
+++ dolar/ftp.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import os
+from ftplib import FTP
+
+
+def place_file(ftp, filename):
+    ftp.storbinary('STOR ' + filename,open(filename, 'rb'))
+
+
+if __name__ == '__main__':
+    url = 'ftp.k-bits.com'
+    ftp = FTP(url)
+    user = 'usuario1@k-bits.com'
+    passw = 'happy1234'
+    ftp.login(user, passw)
+    remoto = []
+    ftp.dir(remoto.append)
+    for r in remoto:
+        print(r)
+    directorio_local = os.listdir('.')
+    place_file(ftp, 'Banamex.csv')
+    print('la wea fome')
+
